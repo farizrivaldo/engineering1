254e34032df35ac8cd3d72da976174912081be0d
farizrivaldo98 <123930917+farizrivaldo98@users.noreply.github.com>
2024-07-30 09:21:09 +0700

    test 1

--- src/pages/MachineHistorical.jsx
+++ src/pages/MachineHistorical.jsx
@@ -1,7 +1,15 @@
 import React from "react";
 
 function MachineHistorical() {
-  return <div>MachineHistorical</div>;
+  return (
+    <>
+      <div>
+        <h1 class="text-center text-4xl antialiased hover:subpixel-antialiased; p-8">
+          REPORT MACHINE BREAKDOWN
+        </h1>
+      </div>
+    </>
+  );
 }
 
 export default MachineHistorical;
