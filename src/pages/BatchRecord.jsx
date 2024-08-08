7d8c459c4eb7147584667ab7a5081f567cb967e3
farizrivaldo98 <123930917+farizrivaldo98@users.noreply.github.com>
2024-08-08 09:31:15 +0700

    useeffect upgrade

--- src/pages/BatchRecord.jsx
+++ src/pages/BatchRecord.jsx
@@ -1,4 +1,4 @@
-import { React, useState } from "react";
+import { React, useState, useEffect } from "react";
 import { Stack, Select } from "@chakra-ui/react";
 import axios from "axios";
 
@@ -87,75 +87,88 @@ function BatchRecord() {
     //console.log(event.target.value);
   };
 
+  useEffect(() => {
+    fetchLine();
+  }, []);
+
   return (
     <>
       <h1 className="text-center text-4xl antialiased hover:subpixel-antialiased p-8">
         BATCH RECORD
       </h1>
+      <div className="pb-12 border-solid border-4 mt-2 ">
+        <div className="flex flex-auto mt-2 gap-x-6 gap-y-8 p-4  sm:grid-cols-6   ">
+          <Stack
+            className="flex flex-row justify-center   "
+            direction="row"
+            spacing={4}
+            align="center"
+          >
+            <div className="main">
+              <h1>Search Batch</h1>
+              <div className="search">
+                <input
+                  id="outlined-basic"
+                  variant="outlined"
+                  fullWidth
+                  label="Search"
+                  className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                />
+              </div>
+              <div className="sm:col-span-4">
+                <label
+                  htmlFor="username"
+                  className="block text-sm font-medium leading-6 text-gray-900"
+                >
+                  Line Area
+                </label>
+                <div className="mt-2 ">
+                  <Select
+                    placeholder="Select Line"
+                    id="line"
+                    onChange={lineHendeler}
+                  >
+                    {renderLine()}
+                  </Select>
+                </div>
+              </div>
+              <div className="sm:col-span-4 ">
+                <label
+                  htmlFor="username"
+                  className="block text-sm font-medium leading-6 text-gray-900"
+                >
+                  Proces
+                </label>
+                <div className="mt-2 ">
+                  <Select
+                    placeholder="Select Machine"
+                    onChange={procesHendeler}
+                  >
+                    {renderProces()}
+                  </Select>
+                </div>
+              </div>
 
-      <Stack
-        className="flex flex-row justify-center   "
-        direction="row"
-        spacing={4}
-        align="center"
-      >
-        <div className="main">
-          <h1>Search Batch</h1>
-          <div className="search">
-            <input
-              id="outlined-basic"
-              variant="outlined"
-              fullWidth
-              label="Search"
-              className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-            />
-          </div>
-          <div className="sm:col-span-4">
-            <label
-              htmlFor="username"
-              className="block text-sm font-medium leading-6 text-gray-900"
-            >
-              Line Area
-            </label>
-            <div className="mt-2 ">
-              <Select
-                placeholder="Select Line"
-                id="line"
-                onChange={lineHendeler}
-              >
-                {renderLine()}
-              </Select>
+              <div className="sm:col-span-4">
+                <label
+                  htmlFor="username"
+                  className="block text-sm font-medium leading-6 text-gray-900"
+                >
+                  Machine
+                </label>
+                <div className="mt-2 ">
+                  <Select
+                    placeholder="Select Machine"
+                    onChange={machineHendeler}
+                  >
+                    {renderMachine()}
+                  </Select>
+                </div>
+              </div>
             </div>
-          </div>
-          <div className="sm:col-span-4 ">
-            <label
-              htmlFor="username"
-              className="block text-sm font-medium leading-6 text-gray-900"
-            >
-              Proces
-            </label>
-            <div className="mt-2 ">
-              <Select placeholder="Select Machine" onChange={procesHendeler}>
-                {renderProces()}
-              </Select>
-            </div>
-          </div>
-
-          <div className="sm:col-span-4">
-            <label
-              htmlFor="username"
-              className="block text-sm font-medium leading-6 text-gray-900"
-            >
-              Machine
-            </label>
-            <div className="mt-2 ">
-              <Select placeholder="Select Machine" onChange={machineHendeler}>
-                {renderMachine()}
-              </Select>
-            </div>
-          </div>
+          </Stack>
         </div>
-      </Stack>
+      </div>
     </>
   );
 }
