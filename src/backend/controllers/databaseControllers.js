4def23287d0e53e86a0caaa69738728d9f4fcfaf
farizrivaldo98 <123930917+farizrivaldo98@users.noreply.github.com>
2024-08-25 12:07:59 +0700

    rev2

--- src/backend/controllers/databaseControllers.js
+++ src/backend/controllers/databaseControllers.js
@@ -2768,7 +2768,7 @@ LEFT JOIN
        REPLACE(REPLACE(REPLACE(REPLACE(CONVERT(data_format_0 USING utf8), '\0', ''), '\b', ''), '$', ''), CHAR(0x00), '') AS data_format_0_string,
        data_format_1,
        data_format_2,
-       data_format_3,
+       data_format_3
 FROM ems_saka.\`cMT-FHDGEA1_EBR_${machine}_data\``;
       console.log("wetmill", querryGet);
     } else {
