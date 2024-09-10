6460ef0adb48143e05b44a2526a0c157da6e17d9
farizrivaldo98 <123930917+farizrivaldo98@users.noreply.github.com>
2024-09-10 14:29:26 +0700

    v3

--- src/pages/BatchRecord.jsx
+++ src/pages/BatchRecord.jsx
@@ -171,7 +171,7 @@ function BatchRecord() {
               //onChange={dateStart}
               placeholder="Select Date and Time"
               size="md"
-              type="dateTime"
+              type="datetime-local"
             />
           </div>
           <div>
@@ -185,7 +185,7 @@ function BatchRecord() {
               //onChange={dateFinish}
               placeholder="Select Date and Time"
               size="md"
-              type="dateTime"
+              type="datetime-local"
             />
           </div>
 
