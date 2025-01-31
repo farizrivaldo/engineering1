73d90e5fb7cc28ff2a0d84cc5be85cdf84b2722d
Henry Rahmadhana <henryalif18@gmail.com>
2025-01-31 08:11:48 +0700

    bubarkan mqtt
    sah ni bos

--- src/backend/index.js
+++ src/backend/index.js
@@ -66,196 +66,196 @@ app.post("/upload", upload.single("file"), async (req, res) => {
 //========================MQTT===============================================================================
 //========================MQTT===============================================================================
 // Konfigurasi broker MQT // Pastikan Anda telah menginstal package 'mqtt' dengan `npm install mqtt`
-const mqttBroker = "mqtt://10.126.15.7"; // Alamat broker Anda
-const mqttTopic1 = "kwhmeter"; // Topik yang ingin di-subscribe
-const mqttTopic2 = "dbwater"; // Topik yang ingin di-subscribe
-const mqttTopic3 = "totalgas"; // Topik yang ingin di-subscribe
-const mqttTopic4 = "masterbox"; // Topik yang ingin di-subscribe
+// const mqttBroker = "mqtt://10.126.15.7"; // Alamat broker Anda
+// const mqttTopic1 = "kwhmeter"; // Topik yang ingin di-subscribe
+// const mqttTopic2 = "dbwater"; // Topik yang ingin di-subscribe
+// const mqttTopic3 = "totalgas"; // Topik yang ingin di-subscribe
+// const mqttTopic4 = "masterbox"; // Topik yang ingin di-subscribe
 
-// Hubungkan ke broker MQTT
-const mqttClient = mqtt.connect(mqttBroker);
-EventEmitter.defaultMaxListeners = 20;
-mqttClient.on("connect", () => {
-  console.log("Terhubung ke broker MQTT");
-  // Subscribe ke topik
-  mqttClient.subscribe(mqttTopic1, (err) => {
-    if (!err) {
-      console.log(`Berhasil subscribe ke topik: ${mqttTopic1}`);
-    } else {
-      console.error("Gagal subscribe ke topik:", err);
-    }
-  });
+// // Hubungkan ke broker MQTT
+// const mqttClient = mqtt.connect(mqttBroker);
+// EventEmitter.defaultMaxListeners = 20;
+// mqttClient.on("connect", () => {
+//   console.log("Terhubung ke broker MQTT");
+//   // Subscribe ke topik
+//   mqttClient.subscribe(mqttTopic1, (err) => {
+//     if (!err) {
+//       console.log(`Berhasil subscribe ke topik: ${mqttTopic1}`);
+//     } else {
+//       console.error("Gagal subscribe ke topik:", err);
+//     }
+//   });
 
-  mqttClient.subscribe(mqttTopic2, (err) => {
-    if (!err) {
-      console.log(`Berhasil subscribe ke topik: ${mqttTopic2}`);
-    } else {
-      console.error("Gagal subscribe ke topik:", err);
-    }
-  });
+//   mqttClient.subscribe(mqttTopic2, (err) => {
+//     if (!err) {
+//       console.log(`Berhasil subscribe ke topik: ${mqttTopic2}`);
+//     } else {
+//       console.error("Gagal subscribe ke topik:", err);
+//     }
+//   });
 
-  mqttClient.subscribe(mqttTopic3, (err) => {
-    if (!err) {
-      console.log(`Berhasil subscribe ke topik: ${mqttTopic3}`);
-    } else {
-      console.error("Gagal subscribe ke topik:", err);
-    }
-  });
+//   mqttClient.subscribe(mqttTopic3, (err) => {
+//     if (!err) {
+//       console.log(`Berhasil subscribe ke topik: ${mqttTopic3}`);
+//     } else {
+//       console.error("Gagal subscribe ke topik:", err);
+//     }
+//   });
 
-  mqttClient.subscribe(mqttTopic4, (err) => {
-    if (!err) {
-      console.log(`Berhasil subscribe ke topik: ${mqttTopic4}`);
-    } else {
-      console.error("Gagal subscribe ke topik:", err);
-    }
-  });
-});
+//   mqttClient.subscribe(mqttTopic4, (err) => {
+//     if (!err) {
+//       console.log(`Berhasil subscribe ke topik: ${mqttTopic4}`);
+//     } else {
+//       console.error("Gagal subscribe ke topik:", err);
+//     }
+//   });
+// });
 
-// Tangani error jika ada
-mqttClient.on("error", (err) => {
-  console.error("Error MQTT:", err);
-});
+// // Tangani error jika ada
+// mqttClient.on("error", (err) => {
+//   console.error("Error MQTT:", err);
+// });
 
-// Event ketika menerima pesan dari topik
-mqttClient.on("message", (topic, message) => {
-  // console.log(`Pesan diterima dari topik "${topic}": ${message.toString()}`);
-  // console.log(mqttClient.listenerCount("message"));
-});
+// // Event ketika menerima pesan dari topik
+// mqttClient.on("message", (topic, message) => {
+//   // console.log(`Pesan diterima dari topik "${topic}": ${message.toString()}`);
+//   // console.log(mqttClient.listenerCount("message"));
+// });
 
-// Buat server WebSocket
-const wss = new WebSocket.Server({ host: "127.0.0.1", port: 8081 });
+// // Buat server WebSocket
+// const wss = new WebSocket.Server({ host: "127.0.0.1", port: 8081 });
 
-wss.on("connection", (ws) => {
-  //console.log("Klien WebSocket terhubung");
+// wss.on("connection", (ws) => {
+//   //console.log("Klien WebSocket terhubung");
 
-  // Kirim pesan selamat datang
-  ws.send("Terhubung ke WebSocket server!");
+//   // Kirim pesan selamat datang
+//   ws.send("Terhubung ke WebSocket server!");
 
-  // // Kirim pesan MQTT yang diterima ke klien WebSocket
-  // mqttClient.on("message", (topic, message) => {
-  //   if (topic === mqttTopic1) {
-  //     console.log(`Pesan dari MQTT: ${message.toString()}`);
-  //     ws.send(`Pesan dari MQTT [${topic}]: ${message.toString()}`);
-  //   }
-  // });
+//   // // Kirim pesan MQTT yang diterima ke klien WebSocket
+//   // mqttClient.on("message", (topic, message) => {
+//   //   if (topic === mqttTopic1) {
+//   //     console.log(`Pesan dari MQTT: ${message.toString()}`);
+//   //     ws.send(`Pesan dari MQTT [${topic}]: ${message.toString()}`);
+//   //   }
+//   // });
 
-  // mqttClient.on("message", (topic, message) => {
-  //   if (topic === mqttTopic2) {
-  //     //console.log(`Pesan dari MQTT: ${message.toString()}`);
-  //     ws.send(`Pesan dari MQTT [${topic}]: ${message.toString()}`);
-  //   }
-  // });
+//   // mqttClient.on("message", (topic, message) => {
+//   //   if (topic === mqttTopic2) {
+//   //     //console.log(`Pesan dari MQTT: ${message.toString()}`);
+//   //     ws.send(`Pesan dari MQTT [${topic}]: ${message.toString()}`);
+//   //   }
+//   // });
 
-  // mqttClient.on("message", (topic, message) => {
-  //   if (topic === mqttTopic3) {
-  //     // console.log(`Pesan dari MQTT: ${message.toString()}`);
-  //     ws.send(`Pesan dari MQTT [${topic}]: ${message.toString()}`);
-  //   }
-  // });
+//   // mqttClient.on("message", (topic, message) => {
+//   //   if (topic === mqttTopic3) {
+//   //     // console.log(`Pesan dari MQTT: ${message.toString()}`);
+//   //     ws.send(`Pesan dari MQTT [${topic}]: ${message.toString()}`);
+//   //   }
+//   // });
 
-  // mqttClient.on("message", (topic, message) => {
-  //   if (topic === mqttTopic4) {
-  //     // console.log(`Pesan dari MQTT: ${message.toString()}`);
-  //     ws.send(`Pesan dari MQTT [${topic}]: ${message.toString()}`);
-  //   }
-  // });
+//   // mqttClient.on("message", (topic, message) => {
+//   //   if (topic === mqttTopic4) {
+//   //     // console.log(`Pesan dari MQTT: ${message.toString()}`);
+//   //     ws.send(`Pesan dari MQTT [${topic}]: ${message.toString()}`);
+//   //   }
+//   // });
 
-  const previousValues = {
-    [mqttTopic1]: null,
-    [mqttTopic2]: null,
-    [mqttTopic3]: null,
-    [mqttTopic4]: null,
-  };
+//   const previousValues = {
+//     [mqttTopic1]: null,
+//     [mqttTopic2]: null,
+//     [mqttTopic3]: null,
+//     [mqttTopic4]: null,
+//   };
 
-  mqttClient.on("message", (topic, message) => {
-    try {
-      // Bersihkan pesan dari karakter tak diinginkan
-      const cleanedMessage = message
-        .toString()
-        .replace(/\\x0B|\+/g, "")
-        .replace(/\s|\+/g, "")
-        .replace(/\t/g, "")
-        .replace(/\f/g, "");
+//   mqttClient.on("message", (topic, message) => {
+//     try {
+//       // Bersihkan pesan dari karakter tak diinginkan
+//       const cleanedMessage = message
+//         .toString()
+//         .replace(/\\x0B|\+/g, "")
+//         .replace(/\s|\+/g, "")
+//         .replace(/\t/g, "")
+//         .replace(/\f/g, "");
 
-      // Parse pesan menjadi JSON
-      const parsedMessage = JSON.parse(cleanedMessage);
+//       // Parse pesan menjadi JSON
+//       const parsedMessage = JSON.parse(cleanedMessage);
 
-      if (parsedMessage && parsedMessage.d) {
-        // Ambil nilai sebelumnya jika ada
-        const previousData = previousValues[topic] || {};
+//       if (parsedMessage && parsedMessage.d) {
+//         // Ambil nilai sebelumnya jika ada
+//         const previousData = previousValues[topic] || {};
 
-        // Proses data di dalam properti `d`
-        for (const [key, value] of Object.entries(parsedMessage.d)) {
-          const newValue = value[0];
+//         // Proses data di dalam properti `d`
+//         for (const [key, value] of Object.entries(parsedMessage.d)) {
+//           const newValue = value[0];
 
-          // Gunakan nilai sebelumnya jika `newValue` adalah 0
-          if (newValue === 0 && previousData[key] !== undefined) {
-            parsedMessage.d[key] = [previousData[key]];
-          } else {
-            // Simpan nilai baru ke `previousValues`
-            previousData[key] = newValue;
-          }
-        }
+//           // Gunakan nilai sebelumnya jika `newValue` adalah 0
+//           if (newValue === 0 && previousData[key] !== undefined) {
+//             parsedMessage.d[key] = [previousData[key]];
+//           } else {
+//             // Simpan nilai baru ke `previousValues`
+//             previousData[key] = newValue;
+//           }
+//         }
 
-        // Perbarui nilai sebelumnya untuk topik ini
-        previousValues[topic] = previousData;
+//         // Perbarui nilai sebelumnya untuk topik ini
+//         previousValues[topic] = previousData;
 
-        // Kirim data ke WebSocket
-        ws.send(`Pesan dari MQTT [${topic}]: ${JSON.stringify(parsedMessage)}`);
-      }
-    } catch (error) {
-      console.error(`Error processing MQTT message for topic ${topic}:`, error);
-    }
-  });
+//         // Kirim data ke WebSocket
+//         ws.send(`Pesan dari MQTT [${topic}]: ${JSON.stringify(parsedMessage)}`);
+//       }
+//     } catch (error) {
+//       console.error(`Error processing MQTT message for topic ${topic}:`, error);
+//     }
+//   });
 
-  //=====================================================================
+//   //=====================================================================
 
-  ws.on("message", (msg) => {
-    //console.log(`Pesan dari klien WebSocket: ${msg}`);
-  });
+//   ws.on("message", (msg) => {
+//     //console.log(`Pesan dari klien WebSocket: ${msg}`);
+//   });
 
-  ws.on("close", () => {
-    //console.log("Klien WebSocket terputus");
-  });
-  //================================================================================
-  // wss.on("connection", (ws) => {
-  //   console.log("Klien WebSocket terhubung");
+//   ws.on("close", () => {
+//     //console.log("Klien WebSocket terputus");
+//   });
+//   //================================================================================
+//   // wss.on("connection", (ws) => {
+//   //   console.log("Klien WebSocket terhubung");
 
-  //   ws.send("Terhubung ke WebSocket server!");
+//   //   ws.send("Terhubung ke WebSocket server!");
 
-  //   if (mqttClient.listenerCount("message") === 0) {
-  //     mqttClient.on("message", mqttMessageHandler);
-  //   }
+//   //   if (mqttClient.listenerCount("message") === 0) {
+//   //     mqttClient.on("message", mqttMessageHandler);
+//   //   }
 
-  //   ws.on("close", () => {
-  //     console.log("Klien WebSocket terputus");
-  //     mqttClient.removeListener("message", mqttMessageHandler);
-  //   });
-  // });
-});
+//   //   ws.on("close", () => {
+//   //     console.log("Klien WebSocket terputus");
+//   //     mqttClient.removeListener("message", mqttMessageHandler);
+//   //   });
+//   // });
+// });
 
-setInterval(() => {
-  const listenerCount = mqttClient.listenerCount("message");
-  // console.log(`Jumlah listener: ${listenerCount}`);
+// setInterval(() => {
+//   const listenerCount = mqttClient.listenerCount("message");
+//   // console.log(`Jumlah listener: ${listenerCount}`);
 
-  if (listenerCount >= 300) {
-    console.warn("Listener melebihi batas, melakukan reset...");
+//   if (listenerCount >= 300) {
+//     console.warn("Listener melebihi batas, melakukan reset...");
 
-    // Hapus semua listener
-    mqttClient.removeAllListeners("message");
+//     // Hapus semua listener
+//     mqttClient.removeAllListeners("message");
 
-    // Tambahkan listener baru
-    mqttClient.on("message", handleMessage);
-    ws.on("message");
+//     // Tambahkan listener baru
+//     mqttClient.on("message", handleMessage);
+//     ws.on("message");
 
-    // console.log("Listener telah direset ke 1.");
-  }
-}, 5000); // Periksa setiap 5 detik
+//     // console.log("Listener telah direset ke 1.");
+//   }
+// }, 5000); // Periksa setiap 5 detik
 
-// console.log("Server WebSocket berjalan di ws://localhost:8080");
+// // console.log("Server WebSocket berjalan di ws://localhost:8080");
 
-app.use("/part", databaseRouter);
+// app.use("/part", databaseRouter);
 
-app.listen(port, () => {
-  console.log("SERVER RUNNING IN PORT" + port);
-});
+// app.listen(port, () => {
+//   console.log("SERVER RUNNING IN PORT" + port);
+// });
