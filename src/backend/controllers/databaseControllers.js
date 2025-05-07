0f0d1cf20435b28d2fa248290b0721e16e27f210
kingpermadi <145948776+kingpermadi@users.noreply.github.com>
2025-05-07 13:36:18 +0700

    Update databaseControllers.js

--- src/backend/controllers/databaseControllers.js
+++ src/backend/controllers/databaseControllers.js
@@ -6325,7 +6325,7 @@ HM1Report: async (request, response) => {
   // Helper untuk konversi WIB ke UTC
   function toUTCString(dateStr) {
     const local = new Date(`${dateStr}`);
-    const utc = new Date(local.getTime() - 7 * 3600 * 1000); // kurangi 7 jam (WIB to UTC)
+    const utc = new Date(local.getTime() + 7 * 3600 * 1000); // kurangi 7 jam (WIB to UTC)
     return utc.toISOString().slice(0, 19).replace('T', ' ');
   }
 
@@ -6410,7 +6410,7 @@ HM1Report: async (request, response) => {
     }
 
     // Filter hanya yang durasi 3 menit atau lebih
-    //const filtered = grouped.filter(item => item.total_minutes >= 3);
+    const filtered = grouped.filter(item => item.total_minutes >= 3);
 
     return response.status(200).send(filtered);
   });
